884136283f880921cb71b1429f80e87532b00c6d
root <bekirkpatrick@proton.me>
2025-12-10 06:58:16 +0000

    updated minecraft server to 1.21.11 (which is now controlled via build argument for better rebuild/dependency tracking)

--- minecraft/minecraft_image.tf
+++ minecraft/minecraft_image.tf
@@ -4,5 +4,8 @@ resource "docker_image" "minecraft_image" {
 
   build {
     context = "${path.module}/minecraft_image"
+    build_args = {
+      "SERVER_JAR_URL" = "https://piston-data.mojang.com/v1/objects/64bb6d763bed0a9f1d632ec347938594144943ed/server.jar"
+    }
   }
 }
